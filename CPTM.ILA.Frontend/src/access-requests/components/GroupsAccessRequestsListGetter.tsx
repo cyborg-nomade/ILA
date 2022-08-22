a58238e7c31eeebb4aafefd23b8ff6a21980b122
Uriel Fiori <uriel.fiori@gmail.com>
2022-08-22 15:47:26 -0300

    group access request:  1 group per request + comite approves only for their groups

--- CPTM.ILA.Frontend/src/access-requests/components/GroupsAccessRequestsListGetter.tsx
+++ CPTM.ILA.Frontend/src/access-requests/components/GroupsAccessRequestsListGetter.tsx
@@ -10,61 +10,73 @@ import { useHttpClient } from "../../shared/hooks/http-hook";
 import AccessRequestsList from "./AccessRequestsList";
 
 const GroupsAccessRequestsListGetter = () => {
-  const [accessRequests, setAccessRequests] = useState<AccessRequestDTO[]>([]);
-
-  const { token } = useContext(AuthContext);
-
-  const { isLoading, error, isWarning, sendRequest, clearError } =
-    useHttpClient();
-
-  useEffect(() => {
-    const getGroupsAccessRequests = async () => {
-      const responseData = await sendRequest(
-        `${process.env.REACT_APP_CONNSTR}/access-requests/type/${tipoSolicitacaoAcesso.AcessoAGrupos}`,
-        undefined,
-        undefined,
-        { Authorization: "Bearer " + token }
-      );
-
-      const loadedAccessRequests: AccessRequestDTO[] = responseData.requests;
-      console.log("loadedAccessRequest: ", loadedAccessRequests);
-      setAccessRequests(loadedAccessRequests);
-    };
-
-    getGroupsAccessRequests().catch((error) => {
-      console.log(error);
-    });
-
-    return () => {
-      setAccessRequests([]);
-    };
-  }, [sendRequest, token]);
-
-  if (isLoading) {
-    return (
-      <Row className="justify-content-center">
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      </Row>
+    const [accessRequests, setAccessRequests] = useState<AccessRequestDTO[]>(
+        []
     );
-  }
 
-  return (
-    <React.Fragment>
-      <h2>Requisições de Acesso a Grupos</h2>
-      {error && (
-        <Alert
-          variant={isWarning ? "warning" : "danger"}
-          onClose={clearError}
-          dismissible
-        >
-          {error}
-        </Alert>
-      )}
-      <AccessRequestsList items={accessRequests} />
-    </React.Fragment>
-  );
+    const { token, user } = useContext(AuthContext);
+
+    const { isLoading, error, isWarning, sendRequest, clearError } =
+        useHttpClient();
+
+    useEffect(() => {
+        const getGroupsAccessRequests = async () => {
+            const responseData = await sendRequest(
+                `${process.env.REACT_APP_CONNSTR}/access-requests/type/${tipoSolicitacaoAcesso.AcessoAGrupos}`,
+                undefined,
+                undefined,
+                { Authorization: "Bearer " + token }
+            );
+
+            const loadedAccessRequests: AccessRequestDTO[] =
+                responseData.requests;
+            console.log("loadedAccessRequest: ", loadedAccessRequests);
+            const userGroupNames = user.groups.map((g) => g.nome);
+            const filteredAccessRequests: AccessRequestDTO[] =
+                loadedAccessRequests.filter((ar) =>
+                    userGroupNames.includes(ar.groupNames[0])
+                );
+            if (user.isDPO) {
+                setAccessRequests(loadedAccessRequests);
+            } else {
+                setAccessRequests(filteredAccessRequests);
+            }
+        };
+
+        getGroupsAccessRequests().catch((error) => {
+            console.log(error);
+        });
+
+        return () => {
+            setAccessRequests([]);
+        };
+    }, [sendRequest, token]);
+
+    if (isLoading) {
+        return (
+            <Row className="justify-content-center">
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </Row>
+        );
+    }
+
+    return (
+        <React.Fragment>
+            <h2>Requisições de Acesso a Grupos</h2>
+            {error && (
+                <Alert
+                    variant={isWarning ? "warning" : "danger"}
+                    onClose={clearError}
+                    dismissible
+                >
+                    {error}
+                </Alert>
+            )}
+            <AccessRequestsList items={accessRequests} />
+        </React.Fragment>
+    );
 };
 
 export default GroupsAccessRequestsListGetter;
